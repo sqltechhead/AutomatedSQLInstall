38bbfa802496d30aed9a0ee0c34a1b104594cfb7
dan.connolly08@gmail.com <dan.connolly08@gmail.com>
2023-03-04 21:58:27 +0000

    push changes for the day

--- SQLAutomatedInstall.ps1
+++ SQLAutomatedInstall.ps1
@@ -1,155 +1,57 @@
-If (!(Get-Module sqlserver -ListAvailable)) {
-    Write-Host "[INFO]: Installing sqlserver"
-    Install-Module -Name SQLServer -Scope CurrentUser -Force -AllowClobber
-}
 
-Import-Module sqlserver
-Import-Module SQLPS
-
-Function Get-InstalledSQLInstances {
-    [cmdletbinding()]
-    param()
-
-    $InstalledSQLInstances = Get-ChildItem -Path "SQLSERVER:\SQL\$env:COMPUTERNAME" | Where-Object { $_.Name -eq $env:COMPUTERNAME }
-
-    return $InstalledSQLInstances
-
-}
-
-Function Invoke-SQLMediaDownload{
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $True )]
-        [ValidateNotNullOrEmpty()]
-        [String]$WorkingDirectory,
-        [Parameter(Mandatory = $True )]
-        [ValidateNotNullOrEmpty()]
-        [Int]$SQLMajorVersion,
-        [Parameter(Mandatory = $True )]
-        [ValidateNotNullOrEmpty()]
-        [String]$SQLMajorURI
-    )
-
-    $SQLInstallFile = "$WorkingDirectory\Binaries\SQLDeveloper_MajorVersion_$($SQLMajorVersion).exe"
-    If (!(Test-Path $SQLInstallFile)) {
-        Write-Host "[INFO]: Downloading SQL Executable"
-        Invoke-Webrequest -Uri $SQLMajorURI -OutFile $SQLInstallFile -Verbose
-    }
-    Else {
-        Write-Host "[INFO]: SQL Executable already exists. Continuing."
-    }
-    
-    [Array]$DownloadParams = @(
-        "/Action=Download",
-        "/MEDIAPATH=$WorkingDirectory\Binaries",
-        "/Quiet",
-        "/MediaType=ISO")
-
-    $ExitCode = Start-Process -Wait $SQLInstallFile -Passthru -ArgumentList $DownloadParams
-
-    If ($ExitCode.ExitCode -ne 0) {
-
-        Throw "[ERROR]: Exit code was non zero logs located $InstallLogFileLocation"
-    }
-}
-
-Function Invoke-SQLAutomatedInstall{
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $True )]
-        [ValidateNotNullOrEmpty()]
-        [String]$WorkingDirectory,
-        [Parameter(Mandatory = $True )]
-        [ValidateNotNullOrEmpty()]
-        [Int]$SQLMajorVersion,
-        [Parameter(Mandatory = $True )]
-        [ValidateNotNullOrEmpty()]
-        [String]$SQLInstallFile
-
-        
-    )
-
-    [Array]$DownloadParams = @(
-        "/Action=Install",
-        "/Quiet",
-        "/IAcceptSQLServerLicenseTerms"
-        "/ConfigurationFile=$WorkingDirectory\ConfigurationFiles\SQLMajorVersion$($SQLMajorVersion)Install.ini")
-
-    $ExitCode = Start-Process -Wait $SQLInstallFile -Passthru -ArgumentList $DownloadParams
-
-    If ($ExitCode.ExitCode -ne 0) {
-
-        Throw "[ERROR]: Exit code was non zero logs located $InstallLogFileLocation"
-    }
-    E:\setup.exe /Action=Upgrade /Quiet /IAcceptSQLServerLicenseTerms /ConfigurationFile="C:\SQLInstall\UpgradeConfigurationFile.ini"
-}
-
-Function Invoke-SQLAutomatedUpgrade{
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $True )]
-        [ValidateNotNullOrEmpty()]
-        [String]$WorkingDirectory,
-        [Parameter(Mandatory = $True )]
-        [ValidateNotNullOrEmpty()]
-        [Int]$SQLMajorVersion,
-        [Parameter(Mandatory = $True )]
-        [ValidateNotNullOrEmpty()]
-        [String]$SQLInstallFile
-    )
-
-    [Array]$DownloadParams = @(
-        "/Action=Upgrade",
-        "/QuietSimple",
-        "/IAcceptSQLServerLicenseTerms"
-        "/ConfigurationFile=$WorkingDirectory\ConfigurationFiles\SQLMajorVersion$($SQLMajorVersion)Upgrade.ini")
-
-    $ExitCode = Start-Process -Wait $SQLInstallFile -Passthru -ArgumentList $DownloadParams
-
-    If ($ExitCode.ExitCode -ne 0) {
-
-        Throw "[ERROR]: Exit code was non zero logs located $InstallLogFileLocation"
-    }
-}
-
-$SQLInstallURI = "https://go.microsoft.com/fwlink/p/?linkid=2215158"
+#$SQLInstallURI = "https://go.microsoft.com/fwlink/p/?linkid=2215158"
+$SQLInstallURI = "https://go.microsoft.com/fwlink/?linkid=866662"
 $WorkingDirectory = "C:\Projects\AutomatedSQLInstall\AutomatedSQLInstall"
-$ExpectedMajorVersion = 16 ##SQL 2019
+$ExpectedMajorVersion = 15 ##SQL 2019
 $InstallLogFileLocation = "C:\Program Files\Microsoft SQL Server\$($ExpectedMajorVersion)0\Setup Bootstrap\Log"
 
-$InstalledSQLInstances = Get-InstalledSQLInstances
 
-If ($InstalledSQLInstances.Version.Major -eq $ExpectedMajorVersion) {
-    Write-Host "[INFO]: SQL Server is already at correct version. Exiting."
+Try {
+    $InstalledSQLInstances = Get-DefaultSQLInstance
 
-    return
-}
-Else
-{
-    # Download SQL ISO
-    Invoke-SQLMediaDownload -WorkingDirectory $WorkingDirectory -SQLMajorVersion $ExpectedMajorVersion -SQLMajorURI $SQLInstallURI
+    If ($InstalledSQLInstances.Version.Major -eq $ExpectedMajorVersion) {
+        Write-Host "[INFO]: SQL Server is already at correct version. Exiting."
 
-    $ISOFile = (Get-ChildItem -Path "$WorkingDirectory\Binaries" | Where-Object {$_.Extension -eq ".iso"} | Select-Object -First 1).FullName
-    $ISOMountDiskLetter = (Mount-DiskImage -ImagePath $ISOFile -passthru | Get-Volume).DriveLetter
-
-    If (!$SQLInstance) {
-        Write-Host "[INFO]: No SQL Instance Detected. Installing..."
-
-        Invoke-SQLAutomatedInstall -WorkingDirectory $WorkingDirectory -SQLMajorVersion $ExpectedMajorVersion -SQLInstallFile "$($ISOMountDiskLetter):\setup.exe"
+        return
     }
-    ElseIf ($SQLInstance.Version.Major -lt $ExpectedMajorVersion) {
-        Write-Host "[INFO]: Detected that you are on a lower version of SQL. Upgrading..."
+    ElseIf ($InstalledSQLInstances.Version.Major -gt $ExpectedMajorVersion) {
+        Write-Host "[INFO]: Detected that you are on a higher version of SQL. Uninstall in Installed Apps and rerun this script to install the correct version"
 
-        Invoke-SQLAutomatedUpgrade -WorkingDirectory $WorkingDirectory -SQLMajorVersion $ExpectedMajorVersion -SQLInstallFile "$($ISOMountDiskLetter):\setup.exe"
     }
-    ElseIf ($SQLInstance.Version.Major -gt $ExpectedMajorVersion) {
-        Write-Host "[INFO]: Detected that you are on a higher version of SQL. Uninstalling"
-        Read-Host "[WARNING]: This will cause your instance and databases to be dropped. If you are not ok with this data loss, don't continue."
+    Else {
+    
+        # Download SQL ISO
+        #Invoke-SQLMediaDownload -WorkingDirectory $WorkingDirectory -SQLMajorVersion $ExpectedMajorVersion -SQLMajorURI $SQLInstallURI
 
-        Invoke-SQLAutomatedUninstall -WorkingDirectory $WorkingDirectory -SQLMajorVersion $ExpectedMajorVersion -SQLInstallFile "$($ISOMountDiskLetter):\setup.exe"
+        Write-Host "[INFO]: Mounting ISO File"
+        $ISOFile = (Get-ChildItem -Path "$WorkingDirectory\Binaries" | Where-Object { $_.Extension -eq ".iso" } | Select-Object -First 1).FullName
+        $ISOMountDiskLetter = (Mount-DiskImage -ImagePath $ISOFile -passthru | Get-Volume).DriveLetter
 
-        Invoke-SQLAutomatedInstall -WorkingDirectory $WorkingDirectory -SQLMajorVersion $ExpectedMajorVersion -SQLInstallFile "$($ISOMountDiskLetter):\setup.exe"
-    }
+        If (!$InstalledSQLInstances) {
+            Write-Host "[INFO]: No SQL Instance Detected. Installing..."
+
+            Invoke-SQLAutomatedInstall -WorkingDirectory $WorkingDirectory -SQLMajorVersion $ExpectedMajorVersion -SQLInstallFile "$($ISOMountDiskLetter):\setup.exe"
+
+            Write-Host "[INFO]: Dismounting ISO File"
+            Dismount-DiskImage -ImagePath $ISOFile | Out-Null
+        }
+        ElseIf ($InstalledSQLInstances.Version.Major -lt $ExpectedMajorVersion) {
+            Write-Host "[INFO]: Detected that you are on a lower version of SQL. Upgrading..."
+
+            Invoke-SQLAutomatedUpgrade -WorkingDirectory $WorkingDirectory -SQLMajorVersion $ExpectedMajorVersion -SQLInstallFile "$($ISOMountDiskLetter):\setup.exe"
+
+            Write-Host "[INFO]: Dismounting ISO File"
+            Dismount-DiskImage -ImagePath $ISOFile | Out-Null
+        }
    
-}
+    }
 
+}
+Catch {
+    If (Test-Path $ISOMountDiskLetter) {
+        Write-Host "[INFO]: Dismounting ISO File"
+        Dismount-DiskImage -ImagePath $ISOFile | Out-Null
+
+        Throw $_;
+    }
+}
